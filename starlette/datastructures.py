59c46072e84ee0c5e258006d8e2f9b868221f95f
Nathaniel Sabanski <sabanski.n@gmail.com>
2023-06-10 20:53:17 -0700

    Less anaemic State object

--- starlette/datastructures.py
+++ starlette/datastructures.py
@@ -679,7 +679,6 @@ class MutableHeaders(Headers):
             vary = ", ".join([existing, vary])
         self["vary"] = vary
 
-
 class State:
     """
     An object that can be used to store arbitrary state.
@@ -697,12 +696,29 @@ class State:
     def __setattr__(self, key: typing.Any, value: typing.Any) -> None:
         self._state[key] = value
 
+    def __repr__(self):
+        items = ', '.join(f'{k}: {v}' for k, v in self._state.items())
+        return f'{{{items}}}'
+
+    def __getitem__(self,name):
+        try:
+            return self._state[name]
+        except: 
+            return ''
+
+    def __str__(self):
+        items = ', '.join(f'{k}: {v}' for k, v in self._state.items())
+        return f'{{{items}}}'
+
     def __getattr__(self, key: typing.Any) -> typing.Any:
         try:
             return self._state[key]
         except KeyError:
-            message = "'{}' object has no attribute '{}'"
-            raise AttributeError(message.format(self.__class__.__name__, key))
+            return ''
+            #message = "'{}' object has no attribute '{}'"
+            #raise AttributeError(message.format(self.__class__.__name__, key))
 
     def __delattr__(self, key: typing.Any) -> None:
         del self._state[key]
+
+
